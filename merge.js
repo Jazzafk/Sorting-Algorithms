d1522351328717f3c801a78399c8747075c92071
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-06-04 15:19:36 -0400

    Update merge.js

--- merge.js
+++ merge.js
@@ -9,11 +9,22 @@ var input = rawinput.toLowerCase()
 document.getElementById('merge_input').value = ''
 var arr1 = []
 
+
 for (i=0; i<input.length; i++){
     arr1.push(input[i])
-    console.log(arr1)
+   // console.log(arr1)
 }
 
+var numOfSplits = Math.ceil(Math.sqrt(arr1.length))
+console.log(numOfSplits)
+
+let spl1_1 = arr1.splice(0,arr1.length/2)
+let spl1_2 = (arr1)
+
+for (i=0; i<numOfSplits; i++){
+
+
+ }
 
 
 
