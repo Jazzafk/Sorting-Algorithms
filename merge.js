1ae2e6942f599afcd3918c4a01943e8149cb6e7d
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-06-07 13:12:35 -0400

    merge update

--- merge.js
+++ merge.js
@@ -8,25 +8,85 @@ var rawinput = document.getElementById('merge_input').value
 var input = rawinput.toLowerCase()
 document.getElementById('merge_input').value = ''
 var arr1 = []
+var obj = {}
+
 
 
 for (i=0; i<input.length; i++){
     arr1.push(input[i])
-   // console.log(arr1)
+   //console.log(arr1)
 }
 
 var numOfSplits = Math.ceil(Math.sqrt(arr1.length))
-console.log(numOfSplits)
 
-let spl1_1 = arr1.splice(0,arr1.length/2)
-let spl1_2 = (arr1)
+split(arr1, 1);
 
-for (i=0; i<numOfSplits; i++){
+function split(input, num) {
+    splitArr = []
+    for (let i = 0; i < input.length; i += num) {
+        var letter = input.slice(i, i + num);
+        splitArr.push(letter);
+    }
+    console.log(splitArr)
+
+    if(splitArr%2 !== 1){
+var merge1 = splitArr.length/2 
+    }
+
+    Mergearr = []
+    for(i=0; i<splitArr.length; i++){
+    
+        l1 = splitArr[i][0]
+        l2 = splitArr[i+1][0]
+
+        // Mergearr.push([l1,l2])
+
+if (l1 % 2 != 0){
+
+        if(l1>l2){
+            Mergearr.push([l1,l2])
+        }else{
+            Mergearr.push([l2,l1])
+        }
+}
 
 
- }
+    }
+    console.log(Mergearr)
 
 
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+//console.log(numOfSplits)
+
+// console.log(arr1.splice(0,arr1.length/2))
+// console.log((arr1))
+
+// console.log('l= ' + (arr1.length/2))
+
+// for (i=0; i<numOfSplits; i++){
+
+// window['arr' + i +'_1'] = arr1.slice(0,(arr1.length/2))
+// window['arr'+ i + '_2'] = (arr1)
+
+
+
+//  }
+
+//  console.log(arr0_1)
+//  console.log(arr1)
 
 
 
