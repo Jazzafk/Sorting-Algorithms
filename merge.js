464134dca212e97b70a40b5b7bf81bc2c780b2ea
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-06-11 19:31:10 -0400

    Update merge.js

--- merge.js
+++ merge.js
@@ -12,65 +12,76 @@ var obj = {}
 
 
 
-// for (i=0; i<input.length; i++){
-//     arr1.push(input[i])
-//    //console.log(arr1)
-// }
+for (i=0; i<input.length; i++){
+    arr1.push(input[i])
+   //console.log(arr1)
+}
 
-// var numOfSplits = Math.ceil(Math.sqrt(arr1.length))
+var numOfSplits = Math.ceil(Math.sqrt(arr1.length))
 
-// split(arr1, 1);
+split(arr1, 1);
 
-// function split(input, num) {
-//     splitArr = []
-//     for (let i = 0; i < input.length; i += num) {
-//         var letter = input.slice(i, i + num);
-//         splitArr.push(letter);
-//     }
-//     console.log(splitArr)
+function split(input, num) {
+    splitArr = []
+    for (let i = 0; i < input.length; i += num) {
+        var letter = input.slice(i, i + num);
+        splitArr.push(letter);
+    }
+    console.log(splitArr)
 
-//     if(splitArr%2 !== 1){
-// var merge1 = splitArr.length/2 
-//     }
+    if(splitArr%2 !== 1){
+var merge1 = splitArr.length/2 
+    }
 
-//     Mergearr = []
+    Mergearr = []
 
-//     for(i=0; i<splitArr.length; i++){
+    for(i=0; i<splitArr.length; i++){
 
-//         if (splitArr.length % 2 != 0){
-//             var solo = splitArr.splice(splitArr.length-1, 1)
-//             console.log(solo)
-//         }
+        if (splitArr.length % 2 != 0){
+            var solo = splitArr.splice(splitArr.length-1, 1)
+            console.log(solo)
+        }
 
     
-//         arrpos = i + 1
-//         l1 = splitArr[i]
-//         l2 = splitArr[arrpos]
+        arrpos = i + 1
+        l1 = splitArr[i]
+        l2 = splitArr[arrpos]
       
         
 
-//         // Mergearr.push([l1,l2])
+        // Mergearr.push([l1,l2])
 
-// if (i % 2 == 0){
-//         if(l1<l2){
-//             Mergearr.push([l1[0],l2[0]])
-//         }else{
-//             Mergearr.push([l2[0],l1[0]])
-//         }
-// }
+if (i % 2 == 0){
+        if(l1<l2){
+            Mergearr.push([l1[0],l2[0]])
+        }else{
+            Mergearr.push([l2[0],l1[0]])
+        }
+}
 
-// if(i==splitArr.length-1){
-//     Mergearr.push([solo[0][0]])
-// }
+if(i==splitArr.length-1){
+    Mergearr.push([solo[0][0]])
+}
 
-// }
-// }
+}
+}
 
-// //first merge done
+//first merge done
+console.log(Mergearr)
+
+function merge(){
+var mergesLeft = Mergearr.length/2
 
 
 
-// var mergesLeft = Mergearr.length/2
+
+}
+
+
+
+
+
+
 
 // for (i=0; i<mergesLeft; i++){
 
@@ -90,50 +101,56 @@ var obj = {}
 
 
     
-function merge(arr1, arr2){
+// function merge(arr1, arr2){
 
-    returnArr = []
+//     returnArr = []
 
-for (i=0; i<arr1.length;i++){
+//     if(arr1.length>arr2.length){
+//         var longer = arr1.length
+//     }else{
+//         var longer = arr2.length
+//     }
+
+// for (i=0; i<longer;i++){
     
-
-    var sm1 = arr1[0]
-    var sm2 = arr2[0]
+//     var sm1 = arr1[0]
+//     var sm2 = arr2[0]
     
+// for (i=1; i<arr1.length; i++){
+//          if(arr1[i]<sm1){
+//             sm1 = arr1[i]
+// }
+//     }
 
-    for (i=1; i<arr1.length; i++){
-if(arr1[i]<sm1){
-    sm1 = arr1[i
-}
-    }
-    
+//     for (i=1; i<arr2.length; i++){
+//         if(arr2[i]<sm2){
+//             sm2 = arr2[i]
+//         }
+//             }
 
+//     if (sm1<sm2){
+//         smallest = sm1
+//         returnArr.push(smallest)
+//         returnArr.push(sm2)
+//         pos = arr1.indexOf(smallest)
+//         arr1.splice(pos,1)
+//     }else{
+//         smallest = sm2
+//         returnArr.push(smallest)
+//         returnArr.push(sm1)
+//         pos = arr2.indexOf(smallest)
+//         arr2.splice(pos,1)
+//     }
 
-    for (i=0; i<arr2.length; i++){
-        if(arr1[i]<sm2){
-            sm2 = arr2[i]
-        }
-            }
-
-    if (sm1<sm2){
-        smallest = sm1
-        pos = arr1.indexOf(smallest)
-        arr1.splice(pos,1)
-    }else{
-        smallest = sm2
-        pos = arr2.indexOf(smallest)
-        arr2.splice(pos,1)
-    }
-
-}
-
-    console.log(returnArr)
-}
-
-merge([1, 3, 5], [4,2,6])
+//     console.log(returnArr)
+// }
 
 
 
+// }
+
+
+// merge([5, 3, 1], [4,2,6])
 
 
 
