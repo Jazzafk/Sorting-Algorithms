242f141ecf0b4a11c1023c06c87d7ab327be2bc8
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-06-12 22:49:45 -0400

    MERGE DONEEEEEEE

--- merge.js
+++ merge.js
@@ -7,178 +7,81 @@ function mergeSort(){
 var rawinput = document.getElementById('merge_input').value
 var input = rawinput.toLowerCase()
 document.getElementById('merge_input').value = ''
-var arr1 = []
-var obj = {}
-
-
+var arr = []
 
 for (i=0; i<input.length; i++){
-    arr1.push(input[i])
-   //console.log(arr1)
+temparr = input[i]
+arr.push(temparr)
 }
 
-var numOfSplits = Math.ceil(Math.sqrt(arr1.length))
+var numofmerges = Math.ceil(Math.sqrt(arr.length))
 
-split(arr1, 1);
-
-function split(input, num) {
-    splitArr = []
-    for (let i = 0; i < input.length; i += num) {
-        var letter = input.slice(i, i + num);
-        splitArr.push(letter);
-    }
-    console.log(splitArr)
-
-    if(splitArr%2 !== 1){
-var merge1 = splitArr.length/2 
-    }
-
-    Mergearr = []
-
-    for(i=0; i<splitArr.length; i++){
-
-        if (splitArr.length % 2 != 0){
-            var solo = splitArr.splice(splitArr.length-1, 1)
-            console.log(solo)
-        }
-
-    
-        arrpos = i + 1
-        l1 = splitArr[i]
-        l2 = splitArr[arrpos]
-      
-        
-
-        // Mergearr.push([l1,l2])
-
-if (i % 2 == 0){
-        if(l1<l2){
-            Mergearr.push([l1[0],l2[0]])
-        }else{
-            Mergearr.push([l2[0],l1[0]])
-        }
-}
-
-if(i==splitArr.length-1){
-    Mergearr.push([solo[0][0]])
-}
-
-}
-}
-
-//first merge done
-console.log(Mergearr)
-
-function merge(){
-var mergesLeft = Mergearr.length/2
-
-
-
-
+for(i=0; i<numofmerges+1; i++){
+sort(arr)
 }
 
 
+function sort(array){
+max = (Math.ceil(array.length/2))
+for (j=0; j<max; j++){
+    if(arr.length != 1)
+ merge(array[j], array[j+1], j)
+}
+
+}
 
 
 
+function merge(arr1, arr2, index){
 
+    console.log('sorting ' + arr1+ ' & '+ arr2 + 'with index ' + index)
 
-// for (i=0; i<mergesLeft; i++){
+    finalarr = []
+     index1 = 0
+    index2 = 0
 
-// for(i=0; i<Mergearr.length; i++){
+    if(arr2==undefined){
+        arr.splice(index, 1, arr1)
 
-// }
+    }else{
  
-
-
-
-// }
-
-
-
-    
+ while (index1 < arr1.length && index2 < arr2.length){
+     
+ pos2 = arr2[index2]
+ pos1 = arr1[index1]
  
-
-
-    
-// function merge(arr1, arr2){
-
-//     returnArr = []
-
-//     if(arr1.length>arr2.length){
-//         var longer = arr1.length
-//     }else{
-//         var longer = arr2.length
-//     }
-
-// for (i=0; i<longer;i++){
-    
-//     var sm1 = arr1[0]
-//     var sm2 = arr2[0]
-    
-// for (i=1; i<arr1.length; i++){
-//          if(arr1[i]<sm1){
-//             sm1 = arr1[i]
-// }
-//     }
-
-//     for (i=1; i<arr2.length; i++){
-//         if(arr2[i]<sm2){
-//             sm2 = arr2[i]
-//         }
-//             }
-
-//     if (sm1<sm2){
-//         smallest = sm1
-//         returnArr.push(smallest)
-//         returnArr.push(sm2)
-//         pos = arr1.indexOf(smallest)
-//         arr1.splice(pos,1)
-//     }else{
-//         smallest = sm2
-//         returnArr.push(smallest)
-//         returnArr.push(sm1)
-//         pos = arr2.indexOf(smallest)
-//         arr2.splice(pos,1)
-//     }
-
-//     console.log(returnArr)
-// }
-
-
-
-// }
-
-
-// merge([5, 3, 1], [4,2,6])
-
-
-
-
-
-
-
-//console.log(numOfSplits)
-
-// console.log(arr1.splice(0,arr1.length/2))
-// console.log((arr1))
-
-// console.log('l= ' + (arr1.length/2))
-
-// for (i=0; i<numOfSplits; i++){
-
-// window['arr' + i +'_1'] = arr1.slice(0,(arr1.length/2))
-// window['arr'+ i + '_2'] = (arr1)
-
-
-
-//  }
-
-//  console.log(arr0_1)
-//  console.log(arr1)
-
-
-
-
+ 
+ if(pos1<pos2){
+     finalarr.push(pos1)
+     index1 = index1 + 1
+ } else{
+     finalarr.push(pos2)
+     index2 = index2 + 1
+ }
+ }
+ 
+ if (arr1.slice(index1).length != undefined){
+     for(i=0; i<arr1.slice(index1).length; i++){
+     finalarr.push(arr1.slice(index1)[i])  
+ }
+ }
+ if (arr2.slice(index2).length != undefined){
+     for(i=0; i<arr2.slice(index2).length; i++){
+ finalarr.push(arr2.slice(index2)[i])
+     }
+ }
+ 
+ 
+ arr.splice(index, 2, finalarr)
 }
 
+ //console.log(arr)
+
+ if(arr.length == 1){
+     document.getElementById('merge_result').innerHTML = arr
+ }
+
+ }
+
+ 
+}
